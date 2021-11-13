7b5bf4e40e27fed392ac053670eb16291618ce19
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-14 02:31:39 +0530

    Update alternating_integers.py

--- alternating_integers.py
+++ alternating_integers.py
@@ -1,15 +1,21 @@
-array=[10, -3, -19, 7, -18, 4, 15, -5]
-swap_index=-1
-for index in range (0, len(array)):
-    if array[index]<0:
-        swap_index+=1
-        temp=array[swap_index]
-        array[swap_index]=array[index]
-        array[index]=temp
-if (swap_index<(len(array))/2): swap_index+=1
-for index in range (0, int(len(array)/2)+1, 2):
-    if (swap_index+index>=len(array) or swap_index-index<0): break
-    temp=array[swap_index+index]
-    array[swap_index+index]=array[swap_index-index-1]
-    array[swap_index-index-1]=temp
-print(array)
+array = [ 10, -3, -19, 7, -18, 4, 15, -5 ]
+swap_index = -1
+
+for index in range ( 0, len(array) ) :
+    if array[ index ] < 0 :
+        swap_index += 1
+        temp = array[ swap_index ]
+        array[ swap_index ] = array[ index ]
+        array[ index ] = temp
+        
+if ( swap_index < (len(array))/2 ) : swap_index += 1
+
+for index in range (0, int(len(array)/2)+1, 2) :
+    
+    if ( swap_index + index >= len(array) or swap_index - index < 0 ) : break
+
+    temp = array[ swap_index + index ]
+    array[ swap_index + index ] = array[ swap_index - index - 1 ]
+    array[ swap_index - index - 1 ] = temp
+    
+print (array)
